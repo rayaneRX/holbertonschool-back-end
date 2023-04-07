5b14810dacbd9a8ace9fa890240981714d04a836
rayaneRX <elkerayane@gmail.com>
2023-04-07 12:22:31 +0200

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,16 +8,13 @@ from sys import argv
 api_url = "https://jsonplaceholder.typicode.com"
 
 if __name__ == "__main__":
-    user_info = requests.get(f"{api_url}/users/{argv[1]}")
-    todo_list = requests.get(f"{api_url}/todos?userId={argv[1]}")
+    user_info = requests.get(f"{api_url}/users/{argv[1]}").json()
+    todo_list = requests.get(f"{api_url}/todos?userId{argv[1]}").json()
 
-    user_data = user_info.json()
-    todoDATA = todo_list.json()
-
-    completed = [task for task in todoDATA if task["completed"]]
-    EMPLOYEE_NAME = user_data["name"]
+    completed = [task for task in todo_list if task["completed"]]
+    EMPLOYEE_NAME = user_info["name"]
     NUMBER_OF_DONE_TASKS = len(completed)
-    TOTAL_NUMBER_OF_TASKS = len(todoDATA)
+    TOTAL_NUMBER_OF_TASKS = len(todo_list)
 
     print("Employee {} is done with task ({}/{})".format(
             EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
